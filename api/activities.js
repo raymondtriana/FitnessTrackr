a7aa244ce94118adb4f56360fcf35607ba519a1b
raymond triana <raymondtriana@gmail.com>
2023-12-12 10:04:25 -0500

    GET activity by id

--- api/activities.js
+++ api/activities.js
@@ -1,19 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllActivities, getActivityById, getActivityByName, createActivity, updateActivity, getPublicRoutinesByActivity } = require('../db');
-const { requireUser, requiredNotSent } = require('./utils')
+const {
+  getAllActivities,
+  getActivityById,
+  getActivityByName,
+  createActivity,
+  updateActivity,
+  getPublicRoutinesByActivity,
+} = require("../db");
+const { requireUser, requiredNotSent } = require("./utils");
 
 // GET /api/activities/:activityId/routines
-router.get('/:activityId/routines', async (req, res, next) => {
+router.get("/:activityId/routines", async (req, res, next) => {
   try {
-    const routines = await getPublicRoutinesByActivity({id: req.params.activityId});
-    if(routines) {
+    const routines = await getPublicRoutinesByActivity({
+      id: req.params.activityId,
+    });
+    if (routines) {
       res.send(routines);
     } else {
       next({
-        name: 'NotFound',
-        message: `No Routines found for Activity ${req.params.activityId}`
-      })
+        name: "NotFound",
+        message: `No Routines found for Activity ${req.params.activityId}`,
+      });
     }
   } catch (error) {
     next(error);
@@ -21,66 +30,96 @@ router.get('/:activityId/routines', async (req, res, next) => {
 });
 
 // GET /api/activities
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const activities = await getAllActivities();
     res.send(activities);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
+
+// GET /api/activities/:id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    const activity = await getActivityById(id);
+    res.send(activity);
+  } catch (error) {
+    console.log(error);
+    res.send("ERROR");
+  }
+});
 
 // POST /api/activities
-router.post('/', requireUser, requiredNotSent({requiredParams: ['name', 'description']}), async (req, res, next) => {
-  try {
-    const {name, description} = req.body;
-    const existingActivity = await getActivityByName(name);
-    if(existingActivity) {
-      next({
-        name: 'NotFound',
-        message: `An activity with name ${name} already exists`
-      });
-    } else {
-      const createdActivity = await createActivity({name, description});
-      if(createdActivity) {
-        res.send(createdActivity);
-      } else {
+router.post(
+  "/",
+  requireUser,
+  requiredNotSent({ requiredParams: ["name", "description"] }),
+  async (req, res, next) => {
+    try {
+      const { name, description } = req.body;
+      const existingActivity = await getActivityByName(name);
+      if (existingActivity) {
         next({
-          name: 'FailedToCreate',
-          message: 'There was an error creating your activity'
-        })
+          name: "NotFound",
+          message: `An activity with name ${name} already exists`,
+        });
+      } else {
+        const createdActivity = await createActivity({ name, description });
+        if (createdActivity) {
+          res.send(createdActivity);
+        } else {
+          next({
+            name: "FailedToCreate",
+            message: "There was an error creating your activity",
+          });
+        }
       }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 // PATCH /api/activities/:activityId
-router.patch('/:activityId', requireUser, requiredNotSent({requiredParams: ['name', 'description'], atLeastOne: true}), async (req, res, next) => {
-  try {
-    const {activityId} = req.params;
-    const existingActivity = await getActivityById(activityId);
-    if(!existingActivity) {
-      next({
-        name: 'NotFound',
-        message: `No activity by ID ${activityId}`
-      });
-    } else {
-      const {name, description} = req.body;
-      const updatedActivity = await updateActivity({id: activityId, name, description})
-      if(updatedActivity) {
-        res.send(updatedActivity);
-      } else {
+router.patch(
+  "/:activityId",
+  requireUser,
+  requiredNotSent({
+    requiredParams: ["name", "description"],
+    atLeastOne: true,
+  }),
+  async (req, res, next) => {
+    try {
+      const { activityId } = req.params;
+      const existingActivity = await getActivityById(activityId);
+      if (!existingActivity) {
         next({
-          name: 'FailedToUpdate',
-          message: 'There was an error updating your activity'
-        })
+          name: "NotFound",
+          message: `No activity by ID ${activityId}`,
+        });
+      } else {
+        const { name, description } = req.body;
+        const updatedActivity = await updateActivity({
+          id: activityId,
+          name,
+          description,
+        });
+        if (updatedActivity) {
+          res.send(updatedActivity);
+        } else {
+          next({
+            name: "FailedToUpdate",
+            message: "There was an error updating your activity",
+          });
+        }
       }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 module.exports = router;
--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/fitness-dev';
+const connectionString = process.env.DATABASE_URL || 'https://postgres:password@localhost:5432/fitness-dev';
 
 const client = new Client({
   connectionString,
